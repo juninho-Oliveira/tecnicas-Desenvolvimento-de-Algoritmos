c74d35bf1ce90f69353a086f339e32f04fe4e88a
junior <junior@gmail.com>
2025-10-27 19:45:57 -0300

    add aula10

--- aula-10/aula10.c
+++ aula-10/aula10.c
@@ -1,5 +1,9 @@
 /*
-
+1- crie e grave um arquivo com dados numericos (valores desordenados.)
+2- leia esse dados do arquivos.
+3- permita escolher o tipo de ordenação (Bubble, Insertion, Selection< merge ou Quick sort).
+4- Ordene os dados e salve o resultado  em um novo arquivo.
+5- Permita alterar um valor dentro do arquivo e reodernar novamente. 
 
 
 */
@@ -9,5 +13,5 @@ int main()
 {
     printf("Hello World");
 
-    return 0;
+return 0;
 }
